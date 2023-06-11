1f861acd747ecf7f5f6fdc614763009672afcb33
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-06-11 10:30:32 +0530

    Next JS To Do Website

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { prisma } from './db'
+import { ToDoItem } from '@/components/ToDoItem'
+
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo (id: string, complete: boolean) {
+  "use server"
+  await prisma.todo.update({where: { id }, data: {complete} })
+}
+
+export default async function Home() {
+  const todos = await getTodos()
+  return (
+    <>
+    <header className="flex justify-between 
+    items-center mb-4">
+      <h1 className="text-2xl">Todos</h1>
+      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+      hover:bg-slate-700 focus-within:bg-slate-700 outline-none" 
+      href="/new">New</Link>
+    </header>
+    <ul className="pl-4">
+      {todos.map(todo => (
+        <ToDoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+      ))}
+    </ul>
+    </>
+  )
+}
